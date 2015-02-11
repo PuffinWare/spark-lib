fe6ca907d2522edd7335d84e46d645606158cc73
John Sokel <jsokel@signal.co>
2015-02-10 19:07:23 -0600

    Add C/F get methods, cleanupa and variable renaming

--- temp/TMP3x.cpp
+++ temp/TMP3x.cpp
@@ -3,14 +3,13 @@
 
 TMP3x::TMP3x(int analogPin, int numReadings, long delay) {
   tmpPin = analogPin;
-  avgSize = numReadings;
-  avgDelay = delay;
+  maxReadings = numReadings;
+  readingDelay = delay;
 
   avgIndex = 0;
   curTemp = 0;
-  startupCount = 0;
-  readings = (double*)malloc(avgSize * sizeof(double));
-
+  readingsToAvg = 0;
+  readings = (double*)malloc(maxReadings * sizeof(double));
 }
 
 void TMP3x::poll() {
@@ -19,35 +18,36 @@ void TMP3x::poll() {
                     ? now - lastReading
                     : (UINT32_MAX - lastReading) + now;
 
-  if (dur > avgDelay) {
+  if (dur > readingDelay) {
     read();
     lastReading = now;
   }
 }
 
-int TMP3x::getTemp() {
-  return curTemp;
+int TMP3x::getTempC() {
+  return int(curTemp * 10);
+}
+
+int TMP3x::getTempF() {
+  return int(curTemp * 18) + 320;
 }
 
 void TMP3x::read() {
   double reading = analogRead(tmpPin);
   double volts = (reading * 3.3) / 4095;
   readings[avgIndex++] = (volts - 0.5) * 100;
-  if (avgIndex == avgSize) {
+
+  if (avgIndex == maxReadings) {
     avgIndex = 0;
   }
-  int valsToAvg = 0;
-  if (startupCount < avgSize) {
-    valsToAvg = startupCount;
-    startupCount++;
-  } else {
-    valsToAvg = avgSize;
+  // during startup, there will only be a few readings
+  if (readingsToAvg < maxReadings) {
+    readingsToAvg++;
   }
 
   double newTemp = 0;
-  for (int i=0; i<valsToAvg; i++) {
+  for (int i=0; i<readingsToAvg; i++) {
     newTemp += readings[i];
   }
-  newTemp /= valsToAvg;
-  curTemp = int(newTemp * 10);
+  curTemp = newTemp / readingsToAvg;
 }
--- temp/TMP3x.h
+++ temp/TMP3x.h
@@ -5,17 +5,18 @@ class TMP3x {
   public:
     TMP3x(int analogPin, int numReadings, long delay);
     void poll();
-    int getTemp();
+    int getTempC();    //! Returns temp * 10, 315 = 31.5c
+    int getTempF();    //! Returns temp * 10, 723 = 72.3f
 
   private:
     void read();
     int tmpPin;
-    int curTemp;      //! Current temp to be returend in getTemp()
+    double curTemp;      //! Current temp to be returend in getTemp()
     int avgIndex;     //! Index within reading array
-    int avgSize;      //! Number of readings to average
-    unsigned long avgDelay;     //! Time between readings
+    int maxReadings;
+    int readingsToAvg;
+    unsigned long readingDelay; //! Time between readings
     unsigned long lastReading;  //! When last reading was taken
-    int startupCount; //! Number of readings taken average
     double *readings;
 };
 #endif // TMP3X_H
